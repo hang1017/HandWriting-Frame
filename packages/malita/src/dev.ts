2590a7ee1c53318eec6981540aec6fa5e0a7c9e5
陈书航 <chen.shuhang@iwhalecloud.com>
2022-06-06 16:01:31 +0800

    feat: day7

--- packages/malita/src/dev.ts
+++ packages/malita/src/dev.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import { serve, ServeOnRequestArgs } from "esbuild";
+import { build } from "esbuild";
 import path from "path";
-import {
-  DEFAULT_PORT,
-  DEFAULT_HOST,
-  DEFAULT_OUTDIR,
-  DEFAULT_PLATFORM,
-  DEFAULT_ENTRY_POINT,
-  DEFAULT_BUILD_PORT,
-} from "./constants";
+import { createServer } from "http";
+import portfinder from "portfinder";
+import { createWebSocketServer } from "./server";
+import { DEFAULT_PORT, DEFAULT_HOST, DEFAULT_OUTDIR, DEFAULT_PLATFORM, DEFAULT_ENTRY_POINT } from "./constants";
 
 export const dev = async () => {
   const cwd = process.cwd();
   const app = express();
+
+  const port = await portfinder.getPortPromise({
+    port: DEFAULT_PORT,
+  });
   app.get("/", (_req, res) => {
     res.set("Content-Type", "text/html");
     res.send(`<!DOCTYPE html>
@@ -26,49 +26,49 @@ export const dev = async () => {
     <body>
         <div id="malita">
             <span>loading...</span>
-            <script src='http://${DEFAULT_HOST}:${DEFAULT_BUILD_PORT}/index.js'></script>
+            <script src='/${DEFAULT_OUTDIR}/index.js'></script>
+            <script src='/malita/client.js'></script>
         </div>
     </body>
     </html>
   `);
   });
 
-  app.listen(DEFAULT_PORT, async () => {
+  const malitaServer = createServer(app);
+  const ws = createWebSocketServer(malitaServer);
+
+  const esbuildOutput = path.resolve(cwd, DEFAULT_OUTDIR);
+  app.use(`/${DEFAULT_OUTDIR}`, express.static(esbuildOutput));
+  app.use(`/malita`, express.static(path.resolve(__dirname, "client")));
+
+  const sendMessage = (type: string, data?: any) => {
+    ws.send(JSON.stringify({ type, data }));
+  };
+
+  malitaServer.listen(port, async () => {
     console.log(`App listening at http://${DEFAULT_HOST}:${DEFAULT_PORT}`);
 
     try {
-      const devServe = await serve(
-        {
-          port: DEFAULT_BUILD_PORT,
-          host: DEFAULT_HOST,
-          servedir: DEFAULT_OUTDIR,
-          onRequest: (args: ServeOnRequestArgs) => {
-            if (args.timeInMS) {
-              console.log(`${args.method}: ${args.path} ${args.timeInMS} ms`);
+      await build({
+        bundle: true,
+        outdir: DEFAULT_OUTDIR,
+        platform: DEFAULT_PLATFORM,
+        logLevel: "error",
+        format: "iife",
+        define: {
+          "process.env.NODE_ENV": JSON.stringify("development"),
+        },
+        watch: {
+          onRebuild: (err, res) => {
+            if (err) {
+              console.log(res);
+              return;
             }
+            sendMessage("reload");
           },
         },
-        {
-          bundle: true,
-          outdir: DEFAULT_OUTDIR,
-          platform: DEFAULT_PLATFORM,
-          logLevel: "error",
-          format: "iife",
-          define: {
-            "process.env.NODE_ENV": JSON.stringify("development"),
-          },
-          external: ["esbuild"],
-          entryPoints: [path.resolve(cwd, DEFAULT_ENTRY_POINT)],
-        }
-      );
-
-      process.on("SIGINT", () => {
-        devServe.stop();
-        process.exit(0);
-      });
-      process.on("SIGTERM", () => {
-        devServe.stop();
-        process.exit(1);
+        external: ["esbuild"],
+        entryPoints: [path.resolve(cwd, DEFAULT_ENTRY_POINT)],
       });
     } catch (e) {
       process.exit(1);
