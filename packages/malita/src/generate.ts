bec231baaae5a26d28f640ae7feb9f80d2d40f1c
陈书航 <chen.shuhang@iwhalecloud.com>
2022-06-28 20:18:28 +0800

    feat: two day 20

---
+++ packages/malita/src/generate.ts
@@ -0,0 +1,17 @@
+import { generateFile } from "@umijs/utils";
+import path from "path";
+import { existsSync } from "fs";
+
+export const generate = (args: string[]) => {
+  const [type, name] = args;
+
+  const cwd = process.cwd();
+  const absSrcPath = path.resolve(cwd, "src");
+  const absPagePath = path.join(absSrcPath, "pages");
+  if (existsSync(absPagePath) && type && name) {
+    generateFile({
+      path: path.join(__dirname, "../templates/page"),
+      target: path.join(absPagePath, name),
+    });
+  }
+};
