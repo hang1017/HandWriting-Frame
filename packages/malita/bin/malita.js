4114d64492c9eb4c13a1c44751d83b47dbec5f13
陈书航 <chen.shuhang@iwhalecloud.com>
2022-07-21 10:38:03 +0800

    feat: three day6

--- packages/malita/bin/malita.js
+++ packages/malita/bin/malita.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program } = require("commander");
 
+program
+  .command("help")
+  .alias("-h")
+  .description("cli 介绍")
+  .action((_name, _other) => {
+    console.log("cli help");
+  });
 
-program.command('help').alias('-h').description('cli 介绍').action((name, other) => {
-    console.log('cli help');
-})
+program.version(require("../package.json").version, "-v, -V");
 
-program.version(require('../package.json').version, '-v, -V');
-
+program
+  .command("dev")
+  .description("服务端渲染启动")
+  .action(() => {
+    const { dev } = require("../dist/dev");
+    dev();
+  });
 
 program.parse();
--- packages/malita/src/dev.tsx
+++ packages/malita/src/dev.tsx
@@ -1,6 +1,46 @@
+import express from "express";
+import { build } from "esbuild";
+import path from "path";
+import {
+  DEFAULT_OUTPUT,
+  DEFAULT_POST,
+  DEFAULT_ENTRY_POINTS,
+  DEFAULT_HOST,
+} from "./contants";
 
-const aa = () => {
-    return 'aa';
-}
+export const dev = async () => {
+  const app = express();
+  const cwd = process.cwd();
+  const output = path.resolve(cwd, DEFAULT_OUTPUT);
 
-console.log(aa());
+  app.get("/", function (_req, res) {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+          <body>
+              <div id="root">
+                  <div>loading...</div>
+              </div>
+      
+              <script src="http://${DEFAULT_HOST}:${DEFAULT_POST}/malita/index.js"></script>
+          </body>
+      </html>
+    `);
+  });
+
+  app.use("/malita", express.static(output));
+
+  app.listen(DEFAULT_POST, async () => {
+    await build({
+      bundle: true,
+      outdir: DEFAULT_OUTPUT,
+      platform: "node",
+      external: ["esbuild"],
+      define: {
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      },
+      entryPoints: [path.resolve(cwd, DEFAULT_ENTRY_POINTS)],
+    });
+    console.log(`server start: http://${DEFAULT_HOST}:3000`);
+  });
+};
