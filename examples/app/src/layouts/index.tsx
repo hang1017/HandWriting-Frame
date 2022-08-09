13ca632e68ca04363f8488fbf4b2fab4957c3723
陈书航 <chen.shuhang@iwhalecloud.com>
2022-08-09 10:22:52 +0800

    feat: three day18 antd-mobile

--- examples/app/src/layouts/index.tsx
+++ examples/app/src/layouts/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Button } from "antd-mobile";
 import { useOutletContent } from "@malitajs/keepalive";
 import "./index.css";
 
@@ -9,6 +10,9 @@ const Layout = () => {
   return (
     <div>
       <div className="malita-layout">
+        <div>
+          <Button>this is mobile button</Button>
+        </div>
         {text}layouts, 当前路由：{pathname}
       </div>
       {useOutletContent()}
